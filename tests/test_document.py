996cb2c558bb262e2d0fc04816a69ccef747fdc0
Bruno Rocha <rochacbruno@gmail.com>
2015-12-11 12:21:33 -0200

    added tests configs and new tests

--- tests/test_document.py
+++ tests/test_document.py
@@ -1,133 +1,60 @@
 import pytest
 
-from esengine.document import Document
-from esengine.fields import IntegerField, StringField, FloatField
+# import elasticsearch.helpers as eh
+# from esengine.document import Document
+# from esengine.fields import IntegerField, StringField, FloatField
 from esengine.exceptions import ClientError
 
 
-QUERY = {
-    "query": {
-        "bool": {
-            "must": [
-                {"match": {"name": "Gonzo"}}
-            ]
-        }
-    }
-}
-
-
-class Doc(Document):
-        _index = 'index'
-        _doctype = 'doc_type'
-        id = IntegerField()
-
-
-class DocWithDefaultClient(Doc):
-    id = IntegerField()  # ID hould be inherited
-    document_id = StringField()
-    house_number = IntegerField()
-    height = FloatField()
-    @classmethod
-    def get_es(cls, es):
-        return es or MockES()
-
-
-class MockES(object):
-    test_id = 100
-    test_ids = [100, 101]
-
-    def index(self, *args, **kwargs):
-        assert kwargs['index'] == Doc._index
-        assert kwargs['doc_type'] == Doc._doctype
-        assert kwargs['id'] == self.test_id
-        assert 'body' in kwargs
-        kwargs['created'] = True
-        kwargs['_id'] = self.test_id
-        return kwargs
-
-    def get(self, *args, **kwargs):
-        assert kwargs['index'] == Doc._index
-        assert kwargs['doc_type'] == Doc._doctype
-        assert kwargs['id'] == self.test_id
-        return {
-            '_source': {
-                'id': self.test_id
-            }
-        }
-
-    def search(self, *args, **kwargs):
-        assert kwargs['index'] == Doc._index
-        assert kwargs['doc_type'] == Doc._doctype
-        assert kwargs['size'] == len(self.test_ids)
-        docs = []
-        for id in self.test_ids:
-            doc = {
-                '_source': {
-                    'doc': {
-                        'id': self.test_id
-                    }
-                }
-            }
-            docs.append(doc)
-        return {
-            'hits': {
-                'hits': docs
-            }
-        }
-
-
-def test_build_result():
+def test_build_result(Doc, MockES):
     resp = MockES().search(index='index', doc_type='doc_type', size=2)
     results = Doc.build_result(resp, es=MockES(), size=2)
     for res in results:
+        print res, res.id
         assert res.id in MockES.test_ids
 
 
-def test_doc_search():
+def test_doc_search(Doc, QUERY, MockES):
     docs = Doc.search(QUERY, es=MockES(), size=2)
     for doc in docs:
         assert doc.id in MockES.test_ids
 
 
-def test_document_save():
+def test_document_save(Doc, MockES):
     Doc(id=MockES.test_id).save(es=MockES())
 
 
-def test_get_with_id():
+def test_get_with_id(Doc, MockES):
     assert Doc.get(id=MockES.test_id, es=MockES()).id == MockES.test_id
 
 
-def test_doc_get():
+def test_doc_get(Doc, MockES):
     doc = Doc.get(id=MockES.test_id, es=MockES())
     assert doc.id == MockES.test_id
 
 
-def test_filter_by_ids():
+def test_filter_by_ids(Doc, MockES):
     docs = Doc.filter(ids=MockES.test_ids, es=MockES())
     for doc in docs:
         assert doc.id in MockES.test_ids
 
 
-def test_raise_if_filter_by_ids_and_filters():
+def test_raise_if_filter_by_ids_and_filters(Doc, MockES):
     with pytest.raises(ValueError):
         Doc.filter(ids=MockES.test_ids, es=MockES(), filters={"name": "Gonzo"})
 
-def mock_bulk(es, updates):
-    assert updates == [
-        {
-            '_op_type': 'index',
-            '_index': Doc._index,
-            '_type': Doc._doctype,
-            '_id': doc,
-            'doc': {'id': doc}
-        }
-        for doc in MockES.test_ids
-    ]
+
+def test_update_all(DocWithDefaultClient, QUERY, eh):
+    docs = DocWithDefaultClient.search(QUERY, size=2)
+    DocWithDefaultClient.update_all(docs, document_id=1)
 
 
-def test_save_all():
-    import elasticsearch.helpers as eh
-    eh.bulk = mock_bulk
+def test_delete_all(DocWithDefaultClient, QUERY, eh):
+    docs = DocWithDefaultClient.search(QUERY, size=2)
+    DocWithDefaultClient.delete_all(docs)
+
+
+def test_save_all(Doc, MockES, eh):
     docs = [
         Doc(id=doc)
         for doc in MockES.test_ids
@@ -135,12 +62,12 @@ def test_save_all():
     Doc.save_all(docs, es=MockES())
 
 
-def test_client_not_defined():
+def test_client_not_defined(Doc, MockES):
     doc = Doc(id=MockES.test_id)
     with pytest.raises(ClientError):
         doc.save()
 
-def test_default_client():
+def test_default_client(DocWithDefaultClient, MockES):
     try:
         doc = DocWithDefaultClient(id=MockES.test_id)
         doc.save()
@@ -149,7 +76,29 @@ def test_default_client():
         pytest.fail("Doc has no default connection")
 
 
-def test_default_client_injected():
+def test_get_es_with_invalid_client(Doc):
+    with pytest.raises(ClientError):
+        Doc.get_es(int)
+
+
+def test__es_is_invalid(Doc):
+    class DocWithInvalidES(Doc):
+        _es = int
+    with pytest.raises(ClientError):
+        DocWithInvalidES.get_es(None)
+
+
+def test_unicode_representation(Doc, MockES):
+    doc = Doc(id=MockES.test_id)
+    assert doc.__unicode__() == u"<D {'id': 100}>"
+
+
+def test_str_representation(Doc, MockES):
+    doc = Doc(id=MockES.test_id)
+    assert doc.__str__() == "<D {'id': 100}>"
+
+
+def test_default_client_injected(Doc, MockES):
     try:
         Doc._es = MockES()
         doc = Doc(id=MockES.test_id)
@@ -159,7 +108,7 @@ def test_default_client_injected():
         pytest.fail("Doc has no default connection")
 
 
-def test_default_client_injected_as_lambda():
+def test_default_client_injected_as_lambda(Doc, MockES):
     try:
         Doc._es = classmethod(lambda cls: MockES())
         doc = Doc(id=MockES.test_id)
@@ -169,7 +118,7 @@ def test_default_client_injected_as_lambda():
         pytest.fail("Doc has no default connection")
 
 
-def test_compare_attributed_values_against_fields():
+def test_compare_attributed_values_against_fields(DocWithDefaultClient, MockES):
     doc = DocWithDefaultClient(id=MockES.test_id)
     doc.document_id = 123456
     doc.house_number = "42"
