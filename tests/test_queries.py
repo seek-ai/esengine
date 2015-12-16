2dda8ac9e75df2e3424bc841dd2d1b510c2519f5
Bruno Rocha <rochacbruno@gmail.com>
2015-12-16 19:51:58 -0200

    one more test for query_builder

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -19,11 +19,31 @@ def test_query_must_not_by_ids():
     }
     query = BaseQuery(
         query=Query.bool(
-            must=[
-                Query.bool(
-                    must_not=[Query.ids([25, 26])]
-                )
-            ]
+            must=[Query.bool(must_not=[Query.ids([25, 26])])]
         )
     )
-    assert query.as_dict() == raw_query
+    assert query.dict == raw_query
+
+
+def test_filter_must_terms_must_not_ids():
+    raw_query = {
+        'query': {
+            'filtered': {
+                'filter': {
+                    'bool': {
+                        'must': [
+                            {'terms': {'field': ['this', 'that', 'other']}}
+                        ],
+                        'must_not': [{'ids': {'values': [25, 26]}}]
+                    }
+                }
+            }
+        }
+    }
+    query = BaseQuery(
+        filter=Filter.bool(
+            must=[Filter.terms('field', ['this', 'that', 'other'])],
+            must_not=[Filter.ids([25, 26])]
+        )
+    )
+    assert query.dict == raw_query
