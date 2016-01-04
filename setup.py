16ca40cf7d31c0d0af3814bd208c34c651fdfd5f
Bruno Rocha <rochacbruno@gmail.com>
2016-01-04 18:40:59 -0200

    pip released

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ except (IOError, ImportError):
 
 setup(
     name='esengine',
-    version="0.0.7",
+    version="0.0.8",
     url='https://github.com/catholabs/esengine',
     license='MIT',
     author="Catholabs",
