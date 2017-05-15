a71e085f3a4b2d668e3dd80ff4e6b50c2d0f8225
Eder <ederfmartins@gmail.com>
2017-05-15 11:10:28 -0300

    bump version

--- esengine/__init__.py
+++ esengine/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.18'
+__version__ = '0.0.19'
 from esengine.embedded_document import EmbeddedDocument  # noqa
 from esengine.document import Document  # noqa
 from esengine.mapping import Mapping  # noqa
